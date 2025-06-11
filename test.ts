3087da2cd0b8a2ee5fc18aba0af43536db2e263c
VETRI9876 <113588034+VETRI9876@users.noreply.github.com>
2025-06-11 08:44:26 +0530

    Update test.ts

--- test.ts
+++ test.ts
@@ -2,12 +2,12 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export let options = {
-  vus: 10,         // Number of virtual users
-  duration: '30s', // Duration of test
+  vus: 10,
+  duration: '30s',
 };
 
 export default function () {
-  let res = http.get('http://74.179.220.54'); // webapp-service IP
+  let res = http.get('http://4.157.69.189');
   check(res, {
     'status is 200': (r) => r.status === 200,
   });
